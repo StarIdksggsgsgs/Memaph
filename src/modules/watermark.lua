2c6a4213d327e16d4921aa617c5025d81729f519
StarIdksggsgsgs <hshajsfusfwisfshwowywisgshsgsh@gmail.com>
2026-03-15 14:21:39 -0400

    Update watermark.lua

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -2,7 +2,7 @@
 local Watermark = {}
 
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6.2 | hercules-obfuscator.xyz/discord | hercules-obfuscator.xyz/source]\n" .. code
+    return "--[Obfuscated by Memaph v1.6.2 | https://discord.gg/fgjBAvJu72 ]\n" .. code
 end
 
 return Watermark
